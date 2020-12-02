c7c3d8fa7e79fb40b072f42a62c90fe2ef05b23a
Andi Rosca <andi23rosca@users.noreply.github.com>
2020-12-02 15:29:59 +0000

    Initial parsing of svg

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { INode, parseSync } from "svgson";
+import { parse as parsePath } from "path-ast";
+
+
+function parse(svg: string) {
+    const ast = parseSync(svg);
+
+    function replaceData(ast: INode) {
+        if(ast.name === "path") {
+            if("d" in ast.attributes) {
+                console.log("found")
+                ast.attributes.d = parsePath(ast.attributes.d);
+            }
+        }
+        if(ast.children) {
+            ast.children.forEach(replaceData);
+        }
+    }
+
+    replaceData(ast);
+    console.log(JSON.stringify(ast, null, 2));
+
+    return ast;
+}
+
+
+parse(`
+<svg height="210" width="400">
+<path d="M150 0 L75 200 L225 200 Z" />
+</svg>`)
