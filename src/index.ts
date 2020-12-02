0a3d080555fc2569159c77edc4d23d352d8670d9
Andi Rosca <andi23rosca@users.noreply.github.com>
2020-12-02 15:38:01 +0000

    Better way to replace data attribute

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,19 @@
-import { INode, parseSync } from "svgson";
+import { parseSync } from "svgson";
 import { parse as parsePath } from "path-ast";
 
 
 function parse(svg: string) {
-  const ast = parseSync(svg);
-
-  function replaceData(ast: INode) {
-    if (ast.name === "path") {
-      if ("d" in ast.attributes) {
-        console.log("found")
-        ast.attributes.d = parsePath(ast.attributes.d);
+  const ast = parseSync(svg, {
+    transformNode(node) {
+      if (node.name === "path") {
+        if ("d" in node.attributes) {
+          node.attributes.d = parsePath(node.attributes.d);
+        }
       }
+      return node;
     }
-    if (ast.children) {
-      ast.children.forEach(replaceData);
-    }
-  }
-
-  replaceData(ast);
+  });
   console.log(JSON.stringify(ast, null, 2));
-
   return ast;
 }
 
